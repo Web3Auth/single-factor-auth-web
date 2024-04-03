a5a452b73e1e577b8489fe133dda8509e27d2dcb
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2024-04-03 17:27:15 +0530

    Use jwt-decode instead of atob

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,4 @@
-/**
- * Parses a JWT Token, without verifying the signature.
- * @param token - JWT Token
- * @returns Extracted JSON payload from the token
- */
-export function parseToken(token: string) {
-  const base64Url = token.split(".")[1];
-  const base64 = base64Url.replace("-", "+").replace("_", "/");
-  return JSON.parse(atob(base64 || ""));
+import { jwtDecode, JwtPayload } from "jwt-decode";
+export function parseToken(token: string): JwtPayload {
+  return jwtDecode(token);
 }
