1919d999bd622e274bdc930ae4540b8c243400de
Chaitanya Potti <chaitanya.potti@gmail.com>
2024-04-11 16:31:14 +0800

    upgrade to v8

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,9 @@
 import { safeatob } from "@toruslabs/openlogin-utils";
-export function decodeToken(token: string) {
+
+export function decodeToken<T>(token: string): { header: { alg: string; typ: string; kid?: string }; payload: T } {
   const [header, payload] = token.split(".");
   return {
     header: JSON.parse(safeatob(header)),
-    payload: JSON.parse(safeatob(payload)),
+    payload: JSON.parse(safeatob(payload)) as T,
   };
 }
