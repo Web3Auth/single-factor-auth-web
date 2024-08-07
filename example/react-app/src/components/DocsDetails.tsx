c5e5e7c24c17373a61ee3548e8ca40c477f6249b
Lionell Briones <llenoil@gmail.com>
2024-08-07 13:44:28 +0800

    - remove playground link - add live integration link

--- example/react-app/src/components/DocsDetails.tsx
+++ example/react-app/src/components/DocsDetails.tsx
@@ -6,27 +6,24 @@ function DocsDetails() {
     <Card>
       <div className="mb-4">
         <h3 className="font-semibold text-app-gray-900  mb-1">Experience Web3Auth, first hand</h3>
-        <p className="text-xs text-app-gray-500 ">
-          Browse our full suite of features for your dApp with our docs. Access codes examples for these features by visiting our
-          <a
-            href="https://web3auth.io/customers.html"
-            className="text-xs text-app-primary-600 hover:underline ml-1"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            playground
-          </a>
-          .
-        </p>
+        <p className="text-xs text-app-gray-500 ">Browse our full suite of features for your dApp with our docs.</p>
       </div>
 
       <Button
         href="https://web3auth.io/docs"
-        className="gap-2 w-full border  flex border-app-gray-300 text-app-gray-800 "
+        className="gap-2 w-full border  flex border-app-gray-300 text-app-gray-800 mb-2"
         target="_blank"
         rel="noopener noreferrer"
       >
         Read our docs
+      </Button>
+      <Button
+        href="https://web3auth.io/customers.html"
+        className="gap-2 w-full border  flex border-app-gray-300 text-app-gray-800"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Check out live integrations
       </Button>
     </Card>
   );
